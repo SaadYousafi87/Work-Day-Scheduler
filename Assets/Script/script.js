50022a0a4fb54a0fd2e796353ba9f56bd10a0db1
saadyousafi87 <saad.yousafi87@gmail.com>
2022-10-09 14:14:41 -0400

    added functions to display textarea color, event listener and update textarea

--- Assets/Script/script.js
+++ Assets/Script/script.js
@@ -1,6 +1,50 @@
 
 var currentDate = moment().format("dddd, MMMM Do");
+var currentTime = moment().format("hA");
 var $pEl = $("#currentDay");
+var timeArray = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+var hourArray = ["hour-9", "hour-10", "hour-11", "hour-12", "hour-1", "hour-2", "hour-3", "hour-4", "hour-5"];
 
 $pEl.text(currentDate);
 
+function colorDisplay(){
+    var index = 0;
+    for(var i = 0; i < timeArray.length; i++){
+        if(currentTime == timeArray[i]){
+            index = i;
+        }
+    }
+    
+    for (var i = 0; i < index; i++){
+        $("#"+timeArray[i]).css("background-color", "#D3D3D3");
+    }
+    
+    $("#"+timeArray[index]).css("background-color", "#FF0000");
+    
+    for (var i = index + 1; i < timeArray.length; i++){
+        $("#"+timeArray[i]).css("background-color", "#00FF00");
+    }
+}
+
+function displayMessage(){
+    for(var i = 0; i < timeArray.length; i++){
+        var value = localStorage.getItem(timeArray[i]);
+        $("#"+timeArray[i]).val(value);
+    }
+}
+
+colorDisplay();
+
+document.addEventListener("click", function(event){
+    var child = event.target;
+    for(var i = 0; i < hourArray.length; i++){
+        if(child.matches("#"+hourArray[i])){
+            var $text = $("#"+timeArray[i]).val();
+            if($text.length != 0){
+                localStorage.setItem(timeArray[i], $text);
+            }
+        }
+    }
+});
+
+displayMessage();
